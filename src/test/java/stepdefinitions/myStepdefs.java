15b099f5d02260438fe89b8bfaa2a49e11d32f1c
prajjawal98 <prajjawal.kansal@nashtechglobal.com>
2023-08-17 22:13:54 +0530

    added files

---
+++ src/test/java/stepdefinitions/myStepdefs.java
@@ -0,0 +1,19 @@
+package stepdefinitions;
+
+import org.testng.annotations.Test;
+
+public class myStepdefs {
+
+    @Test
+    public void test1 (){
+        System.out.println("pass1");
+
+    }
+
+   @Test
+    public void test2 (){
+        System.out.println("pass2");
+
+    }
+
+}
